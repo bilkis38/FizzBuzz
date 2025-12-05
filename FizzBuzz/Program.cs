a5bd43311b95f94360e88df4de70bc014b23bfdb
Bilkis Nisa <bilkisnisa38@gmail.com>
2025-12-05 15:56:07 +0700

    upoad fizzbuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//FizzBuzz
+ // Loop dari angka 1 hingga 100
+        for (int i = 1; i <= 100; i++)
+        {
+            // Cek kondisi FizzBuzz
+            if (i % 15 == 0) // Cek apakah habis dibagi 15 (yaitu habis dibagi 3 DAN 5)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0) // Cek apakah habis dibagi 3
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0) // Cek apakah habis dibagi 5
+            {
+                Console.WriteLine("Buzz");
+            }
+            else // Jika tidak habis dibagi 3, 5, atau 15
+            {
+                Console.WriteLine(i);
+            }
+        }
